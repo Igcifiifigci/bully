d2c14d587f92fc9c6bc178270adac5b0a5a7666c
Lnao <89242053+Igcifiifigci@users.noreply.github.com>
2021-12-11 10:44:09 +0530

    Create int.py

---
+++ int.py
@@ -0,0 +1,23 @@
+import logging
+
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+
+from telegram.ext import CommandHandler, InlineQueryHandler, ConversationHandler
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ParseMode
+
+from telegram.ext import Updater, CallbackQueryHandler, CallbackContext , Filters
+
+import random
+
+updater = Updater(token='', use_context=True)
+
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+
+                    level=logging.INFO)
+
+logger = logging.getLogger()
+
+updater.start_polling(drop_pending_updates = True)
